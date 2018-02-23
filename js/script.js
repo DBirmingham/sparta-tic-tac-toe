1fb423b469d14f239f0e90377e719bd3dd926c42
Tech-A03 <tech-a03@MacBook-Air-9.local>
2018-02-23 13:12:28 +0000

    corrected win conditions

--- js/script.js
+++ js/script.js
@@ -11,18 +11,14 @@ function init(){
 	gameData = {
 		player: 'X',
 		arrayX: [],
-		arrayO: []
+		arrayO: [],
+		TicTacToe: false
 	}
 }
 
 init();
 
-var a = $('td').on('click', function(event){
-	$(this).data('num');
-});
-
 $('td').on('click', function(event){
-	console.log($(this).data('num'))
 	if ($(this).html()){
 		$('.gameMessage').html('You can\'t do that')
 	} else {
@@ -32,19 +28,20 @@ $('td').on('click', function(event){
 				$(this).addClass('X');
 				$(this).html('X');
 				$('.gameMessage').html('Welcome to Sparta TicTacToe!');
+				$('.playerTurn').html('It is X\'s turn')
 				gameData.arrayX.push($(this).data('num'));
-				if (/[012]{3}/.test(gameData.arrayX.join('')) || /[345]{3}/.test(gameData.arrayX.join('')) ||/[678]{3}/.test(gameData.arrayX.join('')) ||/[036]{3}/.test(gameData.arrayX.join('')) ||/[147]{3}/.test(gameData.arrayX.join('')) ||/[258]{3}/.test(gameData.arrayX.join('')) ||/[145]{3}/.test(gameData.arrayX.join('')) ||/[245]{3}/.test(gameData.arrayX.join(''))){
+				if (/[012]{3}/.test(gameData.arrayX.join('')) || /[345]{3}/.test(gameData.arrayX.join('')) ||/[678]{3}/.test(gameData.arrayX.join('')) ||/[036]{3}/.test(gameData.arrayX.join('')) ||/[147]{3}/.test(gameData.arrayX.join('')) ||/[258]{3}/.test(gameData.arrayX.join('')) ||/[048]{3}/.test(gameData.arrayX.join('')) ||/[246]{3}/.test(gameData.arrayX.join(''))){
 					$('.gameMessage').html('X WINS')
 				}
-			//xPlay();
 			break;
 			case 'O':
 				gameData.player = 'X';
 				$(this).addClass('O');
 				$(this).html('O');
 				$('.gameMessage').html('Welcome to Sparta TicTacToe!');
+				$('.playerTurn').html('It is O\'s turn')
 				gameData.arrayO.push($(this).data('num'));
-				if (/[012]{3}/.test(gameData.arrayO.join('')) || /[345]{3}/.test(gameData.arrayX.join('')) ||/[678]{3}/.test(gameData.arrayX.join('')) ||/[036]{3}/.test(gameData.arrayX.join('')) ||/[147]{3}/.test(gameData.arrayX.join('')) ||/[258]{3}/.test(gameData.arrayX.join('')) ||/[145]{3}/.test(gameData.arrayX.join('')) ||/[245]{3}/.test(gameData.arrayX.join(''))){
+				if (/[012]{3}/.test(gameData.arrayO.join('')) || /[345]{3}/.test(gameData.arrayO.join('')) ||/[678]{3}/.test(gameData.arrayO.join('')) ||/[036]{3}/.test(gameData.arrayO.join('')) ||/[147]{3}/.test(gameData.arrayO.join('')) ||/[258]{3}/.test(gameData.arrayO.join('')) ||/[048]{3}/.test(gameData.arrayO.join('')) ||/[246]{3}/.test(gameData.arrayO.join(''))){
 					$('.gameMessage').html('O WINS')
 				}
 			break;
@@ -59,13 +56,20 @@ $('td').on('click', function(event){
 // [036]
 // [147]
 // [258]
-// [145]
-// [245]
+// [048]
+// [246]
 
-
-$('#reset').on('click', function(event){
+//On win reset construct
+function reset (){
 	init();
 	$('td').removeClass('X');
 	$('td').removeClass('O');
 	$('td').html('');
-})
+}
+
+// gameData.TicTacToe = true
+// 	if (gameData.TicTacToe) {
+// 		reset();
+// 	}
+
+$('#reset').on('click', reset)
